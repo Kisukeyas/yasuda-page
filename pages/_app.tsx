a95fd2be3a32841542b9dd286dd2c9dd995e3f2e
Kisukeyas <kisuke.yas@gmail.com>
2023-03-25 01:48:09 +0900

    feat: add prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import { theme } from "@/styles/theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import type { AppProps } from "next/app";
+import { theme } from '@/styles/theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,5 +8,5 @@ export default function App({ Component, pageProps }: AppProps) {
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
-  );
+  )
 }
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,14 +1,14 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material'
 
 export const theme = createTheme({
   components: {
     MuiContainer: {
       styleOverrides: {
         root: {
-          maxWidth: "780px",
-          marginTop: "4rem",
+          maxWidth: '780px',
+          marginTop: '4rem',
         },
       },
     },
   },
-});
+})
