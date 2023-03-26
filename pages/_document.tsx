cca89610318b6e2c944d535daa4bda3326bf5fd1
Kisukeyas <kisuke.yas@gmail.com>
2023-03-26 18:18:59 +0900

    feat: add ogp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html>
+      <Head>
+        <link rel='apple-touch-icon' href='/favicons/apple-touch-icon.png' />
+        <link rel='icon' href='/favicons/favicon.ico' />
+        <meta property='og:title' content='kisuke-yas' />
+        <meta property='og:description' content={'kisuke-yas homepage'} />
+        <meta property='og:image' content={`${process.env.API_PATH}/common-meta/ogp.png`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
